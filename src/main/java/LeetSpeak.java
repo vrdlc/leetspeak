80f39c3800b3921ebd0923dd7736de5fa0ec7a46
Michael Gallegos and Ramon de la Cruz <mklgallegos@gmail.com>
2016-02-08 14:47:18 -0800

    set up intial build

---
+++ src/main/java/LeetSpeak.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+
+public class LeetSpeak{
+  public static void main(String[] args) {
+
+
+
+  }
+}
---
+++ src/test/java/LeetSpeakTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class LeetSpeakTest {
+
+  // @Test
+  // public void leetspeak_returnsAStringAsIsWhenNoLeetspeakIsPresent_wordDoesntChange() {
+  //   LeetSpeak leetSpeak = new LeetSpeak();
+  //   assertEquals("happy", leetSpeak.leetspeak("happy"));
+  // }
+}
